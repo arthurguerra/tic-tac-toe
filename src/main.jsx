0dd915b81a9d371f8cecadecf6f76f138fa6289a
Arthur Guerra <arthur.batista@sc.senai.br>
2025-07-10 22:57:40 -0300

    feat: replace App component with TicTacToe game component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import TicTacToe from './TicTacToe.jsx'
-import App from './App.jsx'
+import TicTacToe from './TicTacToe.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TicTacToe />
   </StrictMode>,
 )
