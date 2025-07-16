52681ed41bf7bfee891cfd86356b628fa056808c
Arthur Guerra <arthur.batista@sc.senai.br>
2025-07-16 18:05:07 -0300

    feat: add tabs so the user can navigate to the new Products Table functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Game from './TicTacToe'
+import FilterableProductTable from './FilterableProductTable'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeTab, setActiveTab] = useState('tic-tac-toe')
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="app">
+      <div className="tabs">
+        <button 
+          className={`tab ${activeTab === 'tic-tac-toe' ? 'active' : ''}`}
+          onClick={() => setActiveTab('tic-tac-toe')}>
+          Tic Tac Toe
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button 
+          className={`tab ${activeTab === 'products' ? 'active' : ''}`}
+          onClick={() => setActiveTab('products')}>
+          Products Table
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+      <div className="tab-content">
+        {activeTab === 'tic-tac-toe' && <Game />}
+        {activeTab === 'thinking-in-react' && <FilterableProductTable />}
+      </div>
+    </div>
   )
 }
 
