7933bf5d5b41c2a7901ce4e88f84e1c7b9551683
Arthur Guerra <arthur.batista@sc.senai.br>
2025-07-16 21:32:24 -0300

    feat: implements static Products Table

--- src/FilterableProductTable.jsx
+++ src/FilterableProductTable.jsx
@@ -1,5 +1,87 @@
-function FilterableProductTable() {
-return <div>teste</div>
+function FilterableProductTable({ products }) {
+  return (
+    <div className="flex-direction-column">
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  );
 }
 
-export default FilterableProductTable;
+function SearchBar() {
+  return (
+    <div className="align-left">
+      <input type="text" placeholder="Search..." />
+      <div>
+        <input type="checkbox" />
+        <label htmlFor="">Only show products in stock</label>
+      </div>
+    </div>
+  );
+}
+
+function ProductTable({ products }) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach(product => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow key={product.category} category={product.category} />
+      );
+      lastCategory = product.category;
+    }
+    rows.push(
+      <ProductRow key={product.name} product={product} />
+    );
+  });
+
+  return (
+    <table className="margin-top-20px">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  );
+}
+
+function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">{category}</th>
+    </tr>
+  );
+}
+
+function ProductRow({ product }) {
+  const name = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "red" }}>{product.name}</span>
+  );
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
+export default function ProductApp() {
+  return <FilterableProductTable products={PRODUCTS} />;
+}
